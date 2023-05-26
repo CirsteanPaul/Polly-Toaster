7bd97e198282d9fa19c2aef9631e7e1a273684a5
RovyGames <rbabalean@gmail.com>
2023-05-26 23:36:40 +0300

    Edited mint title

--- src/modules/home/components/mint-section/MintSection.tsx
+++ src/modules/home/components/mint-section/MintSection.tsx
@@ -19,7 +19,7 @@ const MintSection = () => {
         <source src={width < 600 ? mobileBackground : Background} id="hvid" type="video/mp4" />
       </video>
       <div style={{ transform: 'translate(-50%, -50%)' }} className="absolute md:w-full w-1/2 rounded-md top-1/2 left-1/2 bg-overlay items-center flex flex-col gap-6 p-5 px-5">
-        <h1 className="uppercase sm:text-3xl text-6xl text-headline ">Mint your nft</h1>
+        <h1 className="uppercase sm:text-3xl text-6xl text-headline ">Mint your Polly</h1>
         <p className="sm:text-2xl text-3xl text-headline"> 0 / 2000</p>
         <div className="sm:text-2xl flex flex-col gap-1 items-center justify-center">
           <p className="sm:text-xl text-2xl text-headline"> Price</p>
